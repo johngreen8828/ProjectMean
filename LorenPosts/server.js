0538bddb72fe097f4b7c2284553fc4902d484c91
John Green <johngreen8828@yahoo.com>
2017-01-02 16:12:13 -0600

    Monday afternoon

--- LorenPosts/server.js
+++ LorenPosts/server.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-var Post = require('./models/post.js')
+var Post = require('./models/post')
 
 var app = express()
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/posts', function(req, res, next) {
     Post.find(function(err, posts) {
@@ -11,17 +11,28 @@ app.get('/api/posts', function(req, res, next) {
         res.json(posts)
     })
 })
+app.get('/', function(req, res) {
+    res.sendfile('layouts/posts.html')
+})
+
+
 
 app.post('/api/posts', function(req, res, next) {
     var post = new Post({
+        //username: 'Johhny Greengo!',
+        //body: 'Getting Express Off!'
         username: req.body.username,
         body: req.body.body
+
     })
+    console.log(req.body.username)
     post.save(function(err, post) {
         if (err) { return next(err) }
-        res.json(201, post)
+        res.status(201).json(post)
     })
 })
+
+
 app.listen(3000, function() {
     console.log('Server listening on Port', 3000)
 })
